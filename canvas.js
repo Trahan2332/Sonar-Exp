1fd9f54da501fdf784f2afd1a17028d2da49c1e5
Adam <ajtrahan749@gmail.com>
2019-03-17 22:03:58 -0500

    Add methods to Vector2d class, cleaned up code.

--- canvas.js
+++ canvas.js
@@ -6,67 +6,10 @@ canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext('2d');//imports object w/ properties and methods used for drawing shapes,text,images, etc. Must prepend to objects.
 
-// Rectangles ctrl+k+c comments out while ctrl+k+u uncomments
-// ctx.fillStyle = 'rgba(255,0,0,0.5)';
-// ctx.fillRect(0,0,100,100);//(x,y) dist from top left corner
-// ctx.fillStyle = 'rgba(0,255,0,0.5)';
-// ctx.fillRect(250,100,100,100);
-// ctx.fillStyle = 'rgba(0,0,255,0.5)';
-// ctx.fillRect(400,500,50,50);
-
-// Lines
-// ctx.beginPath();
-// ctx.moveTo(0,canvas.height);//beginning pt.
-// ctx.lineTo(canvas.width/2,0.9*canvas.height);
-// ctx.lineTo(canvas.width,canvas.height);//ending or intermediary pt.
-// ctx.strokeStyle = 'rgba(100,100,255,1)';//colors line
-// ctx.stroke();//tells to draw line
 
 
 
-// Arcs & circles
-// ctx.beginPath();//Separates connecting line from final line point.
-// ctx.arc(100,100,25,0,-Math.PI/2,true);//startAngle & endAngle measures CW from +x-axis
-// ctx.stroke();
-// ctx.beginPath();
-// ctx.arc(300,300,45,0,Math.PI * 2,);
-// ctx.stroke();
-
-
-class Circle {
-  constructor(x, y,radius, dx, dy) {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.dx = dx;
-    this.dy = dy;
-  }
-
-    draw ()  {
-    ctx.beginPath();
-    ctx.arc(this.x,this.y,this.radius,0,Math.PI * 2);
-    ctx.fillStyle = 'green';
-    ctx.fill();
-    ctx.stroke();
-  }
-
-  update()  {
-    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
-      this.dx = -this.dx;
-    } 
-  
-    if ( this.y + this.radius > innerHeight || this.y - this.radius < 0) {
-      this.dy = -this.dy;
-    }
-    this.x += this.dx;
-    this.y += this.dy;
-
-    this.draw();
-  }
-}
-
-
-// To generate many circles w/ random properties
+// To generate many circles w/ random pos. & vel.
 let circleArray = [];
 
 for (let i = 0; i < 1; i++) {
@@ -90,39 +33,9 @@ function animate() {
 //animate();
 
 
-// generates random numbers in range [min,max)
-function getRandomArbitrary(min,max) {
-  return Math.random() * (max - min) + min;
-}
+const vector = new Vector2D(3,4);
+const v2 = new Vector2D(1,2);
 
-function generatePoint(num,x,y) {
-  let myArray = [];
-  for (let i = 0; i < num + 1; i++) {
-    myArray.push(ctx.lineTo((i+1) * x/(num+1), getRandomArbitrary(0.9,1) * y));
-    return myArray;
-  }
-}
-
-console.log(ctx.lineTo(canvas.width,canvas.height));
-
-
-// generates ground profile, input num denotes the number of central points
-function drawGround(num) {
-
-  const numOfDivisions = num + 1;// number of divisions
-  const randPercentCanvasHeight1 = getRandomArbitrary(0.9,1);
-  const randPercentCanvasHeight2 = getRandomArbitrary(0.9,1);
-
-  ctx.beginPath();
-  ctx.moveTo(0,canvas.height)
-    for (let i = 0; i < num ; i++) {
-      ctx.lineTo((i+1) * canvas.width/numOfDivisions, getRandomArbitrary(0.8,1) * canvas.height)
-    }
-  
-  //ctx.lineTo(canvas.width/numOfDivisions,randPercentCanvasHeight1*canvas.height);
-  //ctx.lineTo(num * (canvas.width/numOfDivisions),randPercentCanvasHeight2*canvas.height);
-  ctx.lineTo(canvas.width,canvas.height);
-  ctx.stroke();
-}
+console.log(vector.dot(v2));
 
 drawGround(10);
--- helperfunctions.js
+++ helperfunctions.js
@@ -1,4 +1,4 @@
-//Contains classes and functions to be used as modules in canvas
+//Contains classes and functions to be used in canvas
 
 class Vector2D {
   constructor(x,y) {
@@ -7,17 +7,109 @@ class Vector2D {
   }
   
   add(v) {
-    if (v instanceof Vector2D)
-    return new Vector2D(this.x + v.x, this.y + v.y)
+    if (v instanceof Vector2D) {
+      return new Vector2D(this.x + v.x, this.y + v.y)
+    }
+  }
+
+  subtract(v) {
+    if (v instanceof Vector2D) {
+      return new Vector2D(this.x - v.x, this.y - v.y);
+    }
+  }
+
+  magnitude () {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+
+  dot(v) {
+    if (v instanceof Vector2D) {
+      return new Vector2D(this.x * v.x , this.y * v.y);
+    }
   }
 }
 
 
-const v1 = new Vector2D(4,5);
-const v2 = new Vector2D(2,3);
-let w = v1.add(v2);
+class Circle {
+  constructor(x, y,radius, dx, dy) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.dx = dx;
+    this.dy = dy;
+  }
+
+  draw ()  {
+    ctx.beginPath();
+    ctx.arc(this.x,this.y,this.radius,0,Math.PI * 2);
+    ctx.fillStyle = 'green';
+    ctx.fill();
+    ctx.stroke();
+  }
+
+  update()  {
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+    } 
+  
+    if ( this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    }
+    this.x += this.dx;
+    this.y += this.dy;
+
+    this.draw();
+  }
+}
+
+
+// generates random numbers in range [min,max)
+function getRandomArbitrary(min,max) {
+  return Math.random() * (max - min) + min;
+}
 
 
 
 
+// generates ground profile, input num denotes the number of central points
+function drawGround(num) {
 
+  const numOfDivisions = num + 1;// number of divisions
+  const randPercentCanvasHeight1 = getRandomArbitrary(0.9,1);
+  const randPercentCanvasHeight2 = getRandomArbitrary(0.9,1);
+
+  ctx.beginPath();
+  ctx.moveTo(0,canvas.height)
+    for (let i = 0; i < num ; i++) {
+      ctx.lineTo((i+1) * canvas.width/numOfDivisions, getRandomArbitrary(0.8,1) * canvas.height)
+    }
+  
+  //ctx.lineTo(canvas.width/numOfDivisions,randPercentCanvasHeight1*canvas.height);
+  //ctx.lineTo(num * (canvas.width/numOfDivisions),randPercentCanvasHeight2*canvas.height);
+  ctx.lineTo(canvas.width,canvas.height);
+  ctx.stroke();
+}
+
+
+
+/* How to Draw...
+
+// Lines
+ctx.beginPath();
+ctx.moveTo(x,y);//beginning pt.
+ctx.lineTo(x, y);// Connects a line to these endpoints. Chain for more lines
+ctx.stroke();//tells to draw line
+
+// Arcs & circles
+ctx.beginPath();//Always being with this method. Clears old paths and begins a new one.
+ctx.arc(x, y, radius, startAngle, endAngle [, anticlockwise]);//travels CW from +x axis
+ctx.stroke();//Draws the path specified.
+
+// Rectangles 
+ctx.strokeRect(x, y, width, height);//OUTLINES a rectangle according to strokeStyle().
+ctx.fillRect(x, y, width, height);//draws a rectangle that is FILLED according to the current fillStyle.
+
+ctx.strokeStyle = 'color' ;//specifies the color, gradient, or pattern to use for the strokes (outlines) around shapes
+ctx.fillStyle = color; //specifies the color, gradient, or pattern to use inside shapes.
+
+*/
